724dae4a3a8ed69730bdc5ff7567c309366daee7
Alain Armel <ekoroalainarmel@yahoo.fr>
2022-04-08 00:38:58 +0200

    programme fonctionnelle mais sans refactory appuyeé sur la POO. Elle est prévue pour la section suivante

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ import java.util.stream.*;
 
 public class Main {
 
+
     public static <String> List<String> removeDuplicates(List<String> myList) /**méthode de suppression des doublons sur la liste*/
     {
 
@@ -29,19 +30,35 @@ public class Main {
 
     public static <String> List<String> sortList(List<String> newList) {  /** méthode pour trier la liste par ordre croissant*/
 
-        List<String> sortedList = newList.stream().sorted().collect(Collectors.toList());
+        List<String> sortedList = newList.stream().sorted().collect(Collectors.toList()); // méthode permettant de trier une liste
         return sortedList;
     }
 
 
     public static void writingFile(String filepathWriting, List<String> sortedList) throws IOException { /** writing file from an ArrayList. our aim was to write each single ArrayList element onto our new file so we decided to build a function*/
 
-     FileWriter writer = new FileWriter(filepathWriting); //créer un fichier de sortie à partir de la liste triée
+        FileWriter writer = new FileWriter(filepathWriting); //créer un fichier de sortie à partir de la liste triée
         for (int index = 0; index < sortedList.size(); index++) { //parcourir la liste triée
             writer.write(sortedList.get(index) + System.lineSeparator());// écrire chaque ligne de la liste sur le fichier
         }
         writer.close();
     }
+
+   /* public static int count(List<String> myList) {
+        int nbreOccurrence=0;
+        for (int index = 0; index < myList.size(); index++)
+            nbreOccurrence += Collections.frequency(myList,myList.get(index));
+
+        return nbreOccurrence;
+    } */
+
+            /*public static  <String> List<String> readSymptomFromFile(String filepath) {
+
+            List<String> myList = new ArrayList<String>(Files.readAllLines(Paths.get("filepath")));
+            return myList;
+
+    }*/
+
 
 
     public static void main(String[] args) throws IOException {  // drive function
@@ -49,11 +66,14 @@ public class Main {
         //lire le fichier puis le convertir en une liste
         List<String> myList = new ArrayList<String>(Files.readAllLines(Paths.get("C:\\Users\\Falcon\\Desktop\\Project2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt")));
 
-        // Retirer les doublons
+        //List<String> myList=readSymptomFromFile("C:\\Users\\Falcon\\Desktop\\Project2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+
+         // Retirer les doublons
         List<String> newList = removeDuplicates(myList); // appel de la méthode de dédoublement
 
         System.out.println("ArrayList with duplicates removed:");//afficher la nouvelle liste sans doublon
         System.out.println(" ");
+
 
         for (int index = 0; index < myList.size(); index++) {// parcourir l'ancienne liste pour
 
@@ -61,12 +81,12 @@ public class Main {
 
             List<String> sortedList = sortList(newList); //appel de la méthode pour ranger les éléments par odre aphabétique (tri)
 
-            System.out.println(sortedList.get(index) + "=" + nbreOccurrence); //afficher la liste triée
+            System.out.println(sortedList.get(index) + "=" + nbreOccurrence);//afficher la liste triée
 
             writingFile("C:\\Users\\Falcon\\Desktop\\Project2\\result.out",sortedList);//on appelle la méthode qui permet d'écrire sur un fichier dépuis une liste
 
         }
-
+        
     }
 
     }
