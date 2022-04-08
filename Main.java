0353d658c8a072e6ab0d6ed0d384beb426cba05c
Alain Armel <ekoroalainarmel@yahoo.fr>
2022-04-08 03:14:29 +0200

    modification du code création d'une méthode permettant de lire le fichier des symptoms et remplir son contenu dans une liste

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 package com.company;
-import com.sun.jdi.connect.Connector;
 
 import java.io.*;
 import java.util.*;
 import java.nio.file.*;
 import java.util.Collections;
 import java.util.stream.Collectors;
-import java.util.stream.*;
+
 
 public class Main {
 
+
+    public static <String> List<String> readSymptomFromFile(String filepath)throws IOException{ /**lecture des symptomes d'un fichier texte et les mettre dans une liste*/
+
+        List <String> myList = new ArrayList<String>((Collection<? extends String>) Files.readAllLines(Paths.get("C:\\Users\\Falcon\\Desktop\\Project2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt")));
+        return myList;
+    }
 
     public static <String> List<String> removeDuplicates(List<String> myList) /**méthode de suppression des doublons sur la liste*/
     {
@@ -23,7 +28,6 @@ public class Main {
                 newList.add(myList.get(index));  //ajouter l'élément de l'index de la liste myListe dans la nouvelle liste (newList)
             }
         }
-
         return newList;  // retourner la nouvelle liste
     }
 
@@ -35,7 +39,7 @@ public class Main {
     }
 
 
-    public static void writingFile(String filepathWriting, List<String> sortedList) throws IOException { /** writing file from an ArrayList. our aim was to write each single ArrayList element onto our new file so we decided to build a function*/
+    public static void writingFile(String filepathWriting, List<String> sortedList) throws IOException { /** writing file from an ArrayList our aim was to write each single ArrayList element onto our new file so we decided to build a function*/
 
         FileWriter writer = new FileWriter(filepathWriting); //créer un fichier de sortie à partir de la liste triée
         for (int index = 0; index < sortedList.size(); index++) { //parcourir la liste triée
@@ -52,32 +56,22 @@ public class Main {
         return nbreOccurrence;
     } */
 
-            /*public static  <String> List<String> readSymptomFromFile(String filepath) {
-
-            List<String> myList = new ArrayList<String>(Files.readAllLines(Paths.get("filepath")));
-            return myList;
-
-    }*/
-
-
 
     public static void main(String[] args) throws IOException {  // drive function
 
-        //lire le fichier puis le convertir en une liste
-        List<String> myList = new ArrayList<String>(Files.readAllLines(Paths.get("C:\\Users\\Falcon\\Desktop\\Project2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt")));
+        //lire le fichier puis le convertir en une liste (produire une méthode)
+        //List<String> myList = new ArrayList<String>(Files.readAllLines(Paths.get("C:\\Users\\Falcon\\Desktop\\Project2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt")));
 
-        //List<String> myList=readSymptomFromFile("C:\\Users\\Falcon\\Desktop\\Project2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+        List<String> myList=readSymptomFromFile("C:\\Users\\Falcon\\Desktop\\Project2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
 
-         // Retirer les doublons
         List<String> newList = removeDuplicates(myList); // appel de la méthode de dédoublement
 
         System.out.println("ArrayList with duplicates removed:");//afficher la nouvelle liste sans doublon
         System.out.println(" ");
 
-
         for (int index = 0; index < myList.size(); index++) {// parcourir l'ancienne liste pour
 
-            int nbreOccurrence = Collections.frequency(myList, myList.get(index));//compter les nombres d'occurrences de chaque symptome
+            int nbreOccurrence = Collections.frequency(myList, myList.get(index));//compter les nombres d'occurrences de chaque symptome (produire une méthode)
 
             List<String> sortedList = sortList(newList); //appel de la méthode pour ranger les éléments par odre aphabétique (tri)
 
@@ -86,7 +80,7 @@ public class Main {
             writingFile("C:\\Users\\Falcon\\Desktop\\Project2\\result.out",sortedList);//on appelle la méthode qui permet d'écrire sur un fichier dépuis une liste
 
         }
-        
+
     }
 
     }
